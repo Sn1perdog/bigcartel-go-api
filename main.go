83431e2204e6d532c662137dad9dc3e5f43905eb
Michael <michael.hoeykens@gmail.com>
2024-07-17 12:50:35 +0200

    refactor: pass client creds instead of from Env

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package bigcartel
 
 import (
-	"log"
 	"net/http"
-	"os"
 )
 
 type Client struct {
@@ -16,23 +14,14 @@ type Client struct {
 	Password   string
 }
 
-func NewClient() *Client {
-	bigcartelUserAgent1 := os.Getenv("BIGCARTEL_USER_AGENT_1")
-	bigcartelUserAgent2 := os.Getenv("BIGCARTEL_USER_AGENT_2")
-	bigcartelAccountNumber := os.Getenv("BIGCARTEL_ACCOUNT_NUMBER")
-	bigcartelUsername := os.Getenv("BIGCARTEL_USERNAME")
-	bigcartelPassword := os.Getenv("BIGCARTEL_PASSWORD")
-
-	if bigcartelUserAgent1 == "" || bigcartelUserAgent2 == "" || bigcartelAccountNumber == "" || bigcartelUsername == "" || bigcartelPassword == "" {
-		log.Fatalf("Missing required environment variables")
-	}
-
+func NewClient(apiToken, accountNumber, userAgent1, userAgent2, username, password string) *Client {
 	return &Client{
+		APIToken:   apiToken,
 		HTTPClient: &http.Client{},
-		BaseURL:    "https://api.bigcartel.com/v1/accounts/" + bigcartelAccountNumber,
-		UserAgent1: bigcartelUserAgent1,
-		UserAgent2: bigcartelUserAgent2,
-		Username:   bigcartelUsername,
-		Password:   bigcartelPassword,
+		BaseURL:    "https://api.bigcartel.com/v1/accounts/" + accountNumber,
+		UserAgent1: userAgent1,
+		UserAgent2: userAgent2,
+		Username:   username,
+		Password:   password,
 	}
 }
